061b23906081a90f5aa815011a2f879373c0d148
Ilya Shnayderman <ilyashn@gmail.com>
2026-03-10 14:36:30 +0200

    Publish recall-emulation HTML reports and CSV tables

---
+++ 7e71ecbb7830fd69dc78ba1369a452129b88e980fdbb6d63285fbb8f912f/1.sh
@@ -0,0 +1,2 @@
+sed s/panic/$1/g emulation_panic.html > emulation_$1.html
+echo emulation_per_story/emulation_$1.html
---
+++ 7e71ecbb7830fd69dc78ba1369a452129b88e980fdbb6d63285fbb8f912f/open_all.sh
@@ -0,0 +1,12 @@
+open schissel_v1-pool/changing_radious.html/changing_radious.html
+open open schissel_v2-lake/changing_radious.html
+open boyscout/changing_radious.html
+open triplett_v1-rookie/changing_radious.html
+open triplett_v2-catlady/changing_radious.html
+open hester_v1-park/changing_radious.html
+open hester_v2-church/changing_radious.html
+open panic/changing_radious.html
+open doSomething/changing_radious.html
+open bear/changing_radious.html
+open stein/changing_radious.html
+open costa/changing_radious.html
